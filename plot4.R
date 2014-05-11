850a77a2c6547591f158a0480c2eaeb9cbb9e41d
Raul <raulgc90@gmail.com>
2014-05-11 17:38:36 -0600

    Uploaded wrong .R file for plot 4

--- plot4.R
+++ plot4.R
@@ -1,5 +1,4 @@
 
-
 # Read and clean data
 raw_data <- read.table('household_power_consumption.txt', header=TRUE, 
                    na.strings="?", sep=";", as.is=TRUE)
@@ -16,20 +15,35 @@ data <- subset(full_data, full_data$Date == as.Date('2007-02-01')
 remove(full_data, raw_data)
 
 # Plot
-png(file='plot3.png')
+png(file='plot4.png')
+
+par(mfrow=c(2,2))
+
+# 1,1
+with(data, plot(Time, Global_active_power, type="n", xlab="",
+                ylab="Global Active Power"))
+with(data, lines(Time, Global_active_power))
+
+# 1,2
+with(data, plot(Time, Voltage, type="n", xlab="datetime"))
+with(data, lines(Time, Voltage))
 
 
+# 2,1
 with(data, plot(Time, Sub_metering_1, type="n", xlab="",
      ylab="Energy sub metering"))
 with(data, lines(Time, Sub_metering_1, col="black"))
 with(data, lines(Time, Sub_metering_2, col="red"))
 with(data, lines(Time, Sub_metering_3, col="blue"))
 
-legend("topright", lty=1,
+legend("topright", lty=1, bty="n",
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        col=c("black", "red", "blue"))
 
-dev.off()
+# 2,2
+with(data, plot(Time, Global_reactive_power, type="n", xlab="datetime"))
+with(data, lines(Time, Global_reactive_power))
 
+dev.off()
 
 
